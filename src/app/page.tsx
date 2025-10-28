27558ad2633e66fe2a718f9f4015e4dc6bea1e9b
Kyle Aquino <kpaquino2@up.edu.ph>
2025-10-29 07:42:59 +0800

    chore: Update home page ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
@@ -6,11 +7,14 @@ export default function Home() {
       <div className="flex flex-col items-center gap-4">
         <h1 className="text-3xl font-bold">Welcome to React Playground</h1>
         <p>Edit and play with your own React components!</p>
-        <Link href="/components">
-          <button className="cursor-pointer rounded bg-teal-600 px-4 py-2 text-white transition hover:bg-teal-700">
-            Start
-          </button>
-        </Link>
+        <div className="flex gap-4">
+          <Button asChild>
+            <Link href="/components">Start</Link>
+          </Button>
+          <Button asChild>
+            <Link href="/signin">Sign in</Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
