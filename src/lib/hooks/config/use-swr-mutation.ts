1ee1cab0cfe3253caacc6dd94d9be3da0b35835d
Kyle Aquino <kpaquino2@up.edu.ph>
2025-10-29 12:58:06 +0800

    fix: Revalidate key fix

--- src/lib/hooks/config/use-swr-mutation.ts
+++ src/lib/hooks/config/use-swr-mutation.ts
@@ -1,5 +1,6 @@
 import { AppError } from "@/lib/errors/handler";
 import { createClient } from "@/lib/supabase/client";
+import { useSWRConfig } from "swr";
 import useSWRMutationDefault, {
   type SWRMutationConfiguration,
   SWRMutationResponse,
@@ -15,10 +16,25 @@ export function useSWRMutation<T, Args = any>(
   config?: SWRMutationConfiguration<T, AppError, string, Args>,
 ): SWRMutationResponse<T, AppError, string, Args> {
   const supabase = createClient();
-
+  const { mutate } = useSWRConfig();
   return useSWRMutationDefault<T, AppError, string, Args>(
     key,
     async (_, { arg }: { arg: Args }) => mutator(supabase, arg),
-    config,
+    {
+      ...config,
+      onSuccess: (data, key, c) => {
+        // Revalidate all keys that start with this prefix
+        mutate(
+          (existingkey) => {
+            // Check if key is an array and starts with 'components'
+            return Array.isArray(existingkey) && existingkey[0] === key;
+          },
+          undefined,
+          { revalidate: true },
+        );
+        // Call user's onSuccess if provided
+        config?.onSuccess?.(data, key, c);
+      },
+    },
   );
 }
