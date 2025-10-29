0d7a82dd42cbe9529fd0d39e82fe54d4cd1bf9ac
Kyle Aquino <kpaquino2@up.edu.ph>
2025-10-30 03:26:30 +0800

    chore: Component header match content

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -4,8 +4,8 @@ import { ComponentsList } from "@/components/components/components-list";
 export default function ComponentsPage() {
   return (
     <div className="flex h-full flex-col">
-      <header className="h-12 border-b px-4">
-        <div className="flex h-full items-center justify-between">
+      <header className="h-12 border-b">
+        <div className="mx-auto flex h-full max-w-[332px] items-center justify-between px-4 md:max-w-[648px] lg:max-w-[964px] xl:max-w-[1280px]">
           <p>React Playground</p>
           <AuthStateButton />
         </div>
