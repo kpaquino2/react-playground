a96363959f2a694b5a9669ec7a2aa196e9134d5d
Kyle Aquino <kpaquino2@up.edu.ph>
2025-11-02 13:17:15 +0800

    fix: Component card link

--- src/components/components/component-card.tsx
+++ src/components/components/component-card.tsx
@@ -22,7 +22,7 @@ export function ComponentCard({
   setEditComponent,
 }: ComponentCardProps) {
   return (
-    <Link href={`components/${component.id}`}>
+    <Link href={`c/${component.id}`}>
       <Card className="w-[300px] transition hover:brightness-125">
         <CardHeader>
           <CardTitle>{component.name}</CardTitle>
