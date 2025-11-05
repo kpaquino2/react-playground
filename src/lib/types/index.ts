d7811a8e418b0209640bca3dedc449bde98df8f9
Kyle Aquino <kpaquino2@up.edu.ph>
2025-11-05 21:11:59 +0800

    chore: Cleanup types

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,18 +1,12 @@
 import { type Database } from "./database.types";
 
 export type Component = Database["public"]["Tables"]["components"]["Row"];
-export type ComponentInsert =
-  Database["public"]["Tables"]["components"]["Insert"];
-export type ComponentUpdate =
-  Database["public"]["Tables"]["components"]["Update"];
+export type Profile = Database["public"]["Tables"]["profiles"]["Row"];
 
-export type Collaborator =
-  Database["public"]["Tables"]["component_collaborators"]["Row"];
-
-export type Log = {
+export interface Log {
   type: string;
   message: string;
-};
+}
 
 export interface PreviewSettingsType {
   layout: "center" | "top-left";
